e567742e8e09af3e125a1bcef6832c7b510ffbee
Christian Traintinger <c.traintinger@gmx.net>
2022-12-15 19:44:16 +0100

    Intermediate commit in chapter 16

---
+++ src/main/java/com/polarbookshop/quotefunction/domain/Quote.java
@@ -0,0 +1,3 @@
+package com.polarbookshop.quotefunction.domain;
+
+public record Quote(String content, String author, Genre genre) {}
--- src/test/java/com/polarbookshop/quotefunction/QuoteFunctionApplicationTests.java
+++ src/test/java/com/polarbookshop/quotefunction/QuoteFunctionApplicationTests.java
@@ -1,13 +1,61 @@
 package com.polarbookshop.quotefunction;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+import com.polarbookshop.quotefunction.domain.Genre;
+import com.polarbookshop.quotefunction.domain.Quote;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class QuoteFunctionApplicationTests {
-
+	@Autowired
+	WebTestClient webTestClient;
+	
 	@Test
-	void contextLoads() {
+	void whenAllQuotesThenReturn() {
+		this.webTestClient
+			.get()
+			.uri("/")
+			.exchange()
+			.expectStatus().is2xxSuccessful()
+			.expectBodyList(Quote.class);
 	}
-
+	
+	@Test
+	void whenRandomQuoteThenReturn() {
+		this.webTestClient
+			.get()
+			.uri("/randomQuote")
+			.exchange()
+			.expectStatus().is2xxSuccessful()
+			.expectBody(Quote.class);
+	}
+	
+	@Test
+	void whenGenreQuoteThenReturn() {
+		this.webTestClient
+			.post()
+			.uri("/genreQuote")
+			.bodyValue("FANTASY")
+			.exchange()
+			.expectStatus().is2xxSuccessful()
+			.expectBody(Quote.class)
+			.value(quote -> assertThat(quote.genre()).isEqualTo(Genre.FANTASY));
+	}
+	
+	@Test
+	void whenGenreQuoteLogQuoteThenReturn() {
+		this.webTestClient
+			.post()
+			.uri("/genreQuote,logQuote")
+			.contentType(MediaType.APPLICATION_JSON)
+			.bodyValue("FANTASY")
+			.exchange()
+			.expectStatus().is2xxSuccessful();
+	}
 }
