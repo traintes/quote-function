df417b710c321b5f5f74cdf16bccf94beb5fa4ee
Christian Traintinger <c.traintinger@gmx.net>
2022-12-15 18:22:49 +0100

    Initial commit

---
+++ src/test/java/com/polarbookshop/quotefunction/QuoteFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polarbookshop.quotefunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteFunctionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
